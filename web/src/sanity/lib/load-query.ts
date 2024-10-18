9bfab888d8de02e374f1e7733dc302b391cad5fe
Imran Sulemanji <imran.sulemanji@gmail.com>
2024-10-18 18:35:54 +0100

    add draft perspective

--- web/src/sanity/lib/load-query.ts
+++ web/src/sanity/lib/load-query.ts
@@ -1,6 +1,9 @@
 import { type QueryParams } from "sanity";
 import { sanityClient } from "sanity:client";
 
+const isPreview = process.env.PUBLIC_SANITY_VISUAL_EDITING_ENABLED;
+const perspective = isPreview ? "previewDrafts" : "published";
+
 export async function loadQuery<QueryResponse>({
   query,
   params,
@@ -10,15 +13,18 @@ export async function loadQuery<QueryResponse>({
 }) {
   const { result, resultSourceMap } = await sanityClient.fetch<QueryResponse>(
     query,
+
     params ?? {},
     {
       filterResponse: false,
-      resultSourceMap: "withKeyArraySelector",
+      perspective,
+      resultSourceMap: isPreview ? "withKeyArraySelector" : undefined,
     }
   );
 
   return {
     data: result,
     sourceMap: resultSourceMap,
+    perspective,
   };
 }
