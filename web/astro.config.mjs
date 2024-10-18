b67763605dca6f319386dd398688e01014246d47
Imran Sulemanji <imran.sulemanji@gmail.com>
2024-10-18 23:10:15 +0100

    Update token handling and add perspective property

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -9,24 +9,17 @@ import netlify from "@astrojs/netlify";
 
 const isPreview = process.env.PUBLIC_SANITY_VISUAL_EDITING_ENABLED;
 
-console.log("isPreview", isPreview);
-console.log(
-  "process.env.SANITY_PREVIEW_TOKEN",
-  process.env.SANITY_PREVIEW_TOKEN
-);
-
-if (isPreview) {
-  console.log("Using preview token", process.env.SANITY_PREVIEW_TOKEN);
-}
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     sanity({
       projectId: process.env.SANITY_STUDIO_PROJECT_ID,
       dataset: process.env.SANITY_STUDIO_DATASET,
-      token: isPreview ? process.env.SANITY_PREVIEW_TOKEN : undefined,
+      token: isPreview
+        ? process.env.SANITY_PREVIEW_TOKEN
+        : process.env.SANITY_READ_TOKEN,
       useCdn: !isPreview,
+      perspective: isPreview ? "previewDrafts" : "published",
       apiVersion: "2024-10-17",
     }),
     react(),
